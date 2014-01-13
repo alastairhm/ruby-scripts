d20f5fd634edb6d33b77be8772fcede8a9bfc199
alastairhm <amontgomery@williamhill.co.uk>
2014-01-13 14:52:57 +0000

    updated for new API

--- postTweet.rb
+++ postTweet.rb
@@ -1,7 +1,18 @@
 # Post to Twitter
+# Updated for new Twitter API
 
 require "rubygems"
 require "twitter" #https://github.com/sferik/twitter for config details
+
+require File.expand_path(File.join(File.dirname(__FILE__), "twitterCode.rb"))
+twitUser = "alastair_hm"
+
+myTweet = Twitter::REST::Client.new do |config|
+                config.consumer_key        = @twitterCodes[0]
+                config.consumer_secret     = @twitterCodes[1]
+                config.access_token        = @twitterCodes[2]
+                config.access_token_secret = @twitterCodes[3]
+            end
 
 updateTweet = ARGV[0]
 puts "Raw Tweet [#{updateTweet}]"
@@ -12,7 +23,6 @@ end
 
 # Update your status
 puts "Posting..."
-#Twitter.update(updateTweet)
+myTweet.update(updateTweet)
 sleep(3)
-
-puts Twitter.user_timeline("alastair_hm").first.text
+puts myTweet.user_timeline(twitUser).first.text
