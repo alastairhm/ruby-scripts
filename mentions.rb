5270d39ecf3615848a5fc4999c316baee7b1e014
alastairhm <amontgomery@williamhill.co.uk>
2014-01-13 14:46:58 +0000

    updated for new API

--- mentions.rb
+++ mentions.rb
@@ -1,9 +1,19 @@
 # Find your Twitter Mentions
+# Updated for new Twitter API
 
 require "rubygems"
 require "twitter" #https://github.com/sferik/twitter for config details
 
-Twitter.mentions_timeline.each { |tweet|
+require File.expand_path(File.join(File.dirname(__FILE__), "twitterCode.rb"))
+
+myTweet = Twitter::REST::Client.new do |config|
+                config.consumer_key        = @twitterCodes[0]
+                config.consumer_secret     = @twitterCodes[1]
+                config.access_token        = @twitterCodes[2]
+                config.access_token_secret = @twitterCodes[3]
+            end
+
+myTweet.mentions_timeline.each { |tweet|
     puts "#{tweet.user.screen_name}, #{tweet.text}"
     puts
 }
